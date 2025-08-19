e76b143083a1916d3c95d0b2f4909dd74a226600
Juan Ignacio Silva <juan.ignacio.sil12@gmail.com>
2025-08-19 13:09:06 -0300

    Estilos seccion productos  / home

--- app/views/home/seccion-productos.php
+++ app/views/home/seccion-productos.php
@@ -12,16 +12,48 @@
         <div class="contenedor-cards-productos">
             <div class="card-productos">
                 <div class="card-header">
-                    <div>
-                        
+                    <img src="" alt="">
+                </div>
+                <div class="cuerpo-card">
+                    <h3>OFERTA DEL DÍA</h3>
+                    <h4>Producto</h4>
+                    <p>Descripcion</p>
+                    <p>Vendedor</p>
+                    <div class="calificacion-producto">
+                        5.0
                     </div>
+                    <h2>$ 3000</h2>
                 </div>
             </div>
             <div class="card-productos">
-                
+                <div class="card-header">
+                    <img src="" alt="">
+                </div>
+                <div class="cuerpo-card">
+                    <h3>OFERTA DEL DÍA</h3>
+                    <h4>Producto</h4>
+                    <p>Descripcion</p>
+                    <p>Vendedor</p>
+                    <div class="calificacion-producto">
+                        5.0
+                    </div>
+                    <h2>$ 3000</h2>
+                </div>
             </div>
             <div class="card-productos">
-                
+                <div class="card-header">
+                    <img src="" alt="">
+                </div>
+                <div class="cuerpo-card">
+                    <h3>OFERTA DEL DÍA</h3>
+                    <h4>Producto</h4>
+                    <p>Descripcion</p>
+                    <p>Vendedor</p>
+                    <div class="calificacion-producto">
+                        5.0
+                    </div>
+                    <h2>$ 3000</h2>
+                </div>
             </div>
         </div>
     </div>
