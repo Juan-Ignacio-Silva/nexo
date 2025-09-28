5605748a0bd1434e31d5b8b2946c75063762c6c8
juan-ignacio-silva <juan.ignacio.sil12@gmail.com>
2025-09-28 17:08:08 -0300

    Cambios de estilos y se agrego las reseñas dinamicas para cada producto

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -65,7 +65,7 @@
     <nav class="navegacion">
         <div class="contenedor-navegacion">
             <a href="<?= BASE_URL ?>home" class="enlace-navegacion">Inicio</a>
-            <a href="#" class="enlace-navegacion">Categorías</a>
+            <a href="<?= BASE_URL ?>home/categorias" class="enlace-navegacion">Categorías</a>
             <a href="#" class="enlace-navegacion">Ofertas</a>
             <a href="<?= BASE_URL ?>home/vender" class="enlace-navegacion">Vender</a>
             <a href="#" class="enlace-navegacion">Ayuda</a>
